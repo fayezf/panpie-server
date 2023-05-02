2af4a0434e1867fe2df711d415f0d0cff479a9eb
fayezf <mohammadfayezahmed835@gmail.com>
2023-05-02 22:30:22 +0600

    server route setup

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+const chef = require('./data/chef.json');
+
+app.get('/', (req, res) => {
+    res.send('Chef is running')
+});
+
+app.get('/chef', (req, res) => {
+    res.send(chef)
+})
+
+app.listen(port, () => {
+    console.log(`Chef API is running on port: ${port}`)
+})
